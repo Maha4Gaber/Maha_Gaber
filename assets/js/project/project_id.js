18ecca9c88b341990193ae9ff333fcdc9984111d
Maha4Gaber <77496313+Maha4Gaber@users.noreply.github.com>
2023-08-25 12:33:53 +0300

    First Commit

--- assets/js/project/project_id.js
+++ assets/js/project/project_id.js
@@ -1,6 +1,34 @@
 let project_id = localStorage.getItem('Progect_id');
 
 let Project = myprojects.find((i) => i.id == project_id);
+
+let portfolio_title = document.querySelector(".portfolio-title");
+
+portfolio_title.innerHTML = Project.name;
+
+
+
+let portfolio_info = document.querySelector(".portfolio-info");
+
+let info =`
+
+  <h3>Project information</h3>
+            <ul>
+              <li><strong>Category</strong>: ${Project.filter}</li>
+              <li><strong>Project date</strong>:  ${Project.date}</li>
+              <li><strong>Project URL </strong>: <a href=" ${Project.live}">  live Demo</a></li>
+              <li><strong>Project Code Link </strong>: <a href="${Project.gith}">  Code</a></li>
+            </ul>
+            <p> ${Project.desc}" </p>
+
+      `
+
+
+
+portfolio_info.innerHTML = info;
+
+
+
 let swiperWrapper = document.querySelector(".swiper-wrapper");
   
 let projectItmes = Project.imgs.map((itme) => {
